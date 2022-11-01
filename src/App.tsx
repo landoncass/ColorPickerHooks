d8aa8a680cab69b0efd408b75da91d440ec7df89
Landon Cassman <Landon.Cassman@gmail.com>
2022-11-01 19:44:11 -0400

    Added random starting values

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,63 @@ import React from 'react'
 import { useState } from 'react'
 
 export function App() {
-  const [hue, setHue] = useState(180)
-  const [sat, setSat] = useState(50)
-  const [light, setLight] = useState(50)
+  const [hue, setHue] = useState('180')
+  const [sat, setSat] = useState('50')
+  const [light, setLight] = useState('50')
 
-  function handleHueRange() {
-    setHue(hue)
+  function handleHueRange(event: React.FormEvent<HTMLInputElement>) {
+    setHue(event.currentTarget.value)
   }
 
-  function handleSatRange() {
-    setSat(sat + 1)
+  function handleSatRange(event: React.FormEvent<HTMLInputElement>) {
+    setSat(event.currentTarget.value)
   }
 
-  function handleLightRange() {
-    setLight(light + 1)
+  function handleLightRange(event: React.FormEvent<HTMLInputElement>) {
+    setLight(event.currentTarget.value)
   }
 
-  const newBackgroundColor = `hsl(this.hue, this.sat,this.light )`
+  const newBackgroundColor = `hsl(${hue}, ${sat}%,${light}%)`
   const newStyle = { backgroundColor: newBackgroundColor }
 
   return (
-    <div>
+    <div style={newStyle}>
+      <h1>
+        hsl({hue},{sat}%,{light}%)
+      </h1>
       <p>
         Hue
-        <input id="hue" type="range" name="hue" min="0" max="360" />
+        <input
+          id="hue"
+          type="range"
+          name="hue"
+          min="0"
+          max="360"
+          onInput={handleHueRange}
+          value={hue}
+        />
       </p>
       <p>
         Saturation
-        <input id="sat" type="range" min="0" max="100" />
+        <input
+          id="sat"
+          type="range"
+          min="0"
+          max="100"
+          onInput={handleSatRange}
+          value={sat}
+        />
       </p>
       <p>
         Lightness
-        <input id="light" type="range" min="0" max="100" />
+        <input
+          id="light"
+          type="range"
+          min="0"
+          max="100"
+          onInput={handleLightRange}
+          value={light}
+        />
       </p>
     </div>
   )
